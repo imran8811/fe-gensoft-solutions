f267bd07df2f9fd8fbe7f335d2e3e192da3692e6
Mohd Imran <m.imran8811@gmail.com>
2025-03-19 13:25:49 +0500

    changes

--- src/components/shared/service-counter.tsx
+++ src/components/shared/service-counter.tsx
@@ -7,20 +7,20 @@ export default function ServiceCounter() {
           <div className="row">
             <div className="col-lg-3 wow fadeIn" data-wow-delay=".1s">
               <div className="d-flex counter">
-                <h1 className="me-3 text-primary col-2 text-end"><span className="counter-value">50</span>+</h1>
+                <h1 className="me-3 text-primary col-2 text-end"><span className="counter-value">50</span></h1>
                 <h5 className="text-white mt-1 col-10">Team member working across the globe</h5>
               </div>
             </div>
             <div className="col-lg-3 wow fadeIn" data-wow-delay=".3s">
               <div className="d-flex counter">
-                <h1 className="me-3 text-primary col-2 text-end"><span className="counter-value">200</span>+</h1>
-                <h5 className="text-white mt-1 col-10">Enterprise Solutions Developed</h5>
+                <h1 className="me-3 text-primary col-3 text-end"><span className="counter-value">200</span></h1>
+                <h5 className="text-white mt-1 col-9">Enterprise Solutions Developed</h5>
               </div>
             </div>
             <div className="col-lg-3 wow fadeIn" data-wow-delay=".5s">
               <div className="d-flex counter">
-                <h1 className="me-3 text-primary col-2 text-end"><span className="counter-value">120</span></h1>
-                <h5 className="text-white mt-1 col-10">Total clients who love <span className="text-primary">GenSoft Solutions</span></h5>
+                <h1 className="me-3 text-primary col-3 text-end"><span className="counter-value">120</span></h1>
+                <h5 className="text-white mt-1 col-9">Total clients who love <span className="text-primary">GenSoft Solutions</span></h5>
               </div>
             </div>
             <div className="col-lg-3 wow fadeIn" data-wow-delay=".7s">
